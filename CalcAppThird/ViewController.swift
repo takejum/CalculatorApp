6a9b884ed8053cfd0c9dd8caf89b9c43b34a2951
Jumpei Takeshita <takejum.123@gmail.com>
2020-07-14 10:58:02 +0900

    Initial Commit

---
+++ CalcAppThird/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalcAppThird
+//
+//  Created by Jumpei Takeshita on 2020/07/14.
+//  Copyright © 2020 Jumpei Takeshita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
