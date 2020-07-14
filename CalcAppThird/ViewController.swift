2e2d7ba8602e457f1ccca2a267416d2c51474f7d
Jumpei Takeshita <takejum.123@gmail.com>
2020-07-14 15:39:56 +0900

    first commit of simpleCalculator

--- CalcAppThird/ViewController.swift
+++ CalcAppThird/ViewController.swift
@@ -9,12 +9,227 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    enum CalculateStatus {
+        case none, addition, subtraction, multiplication, division
+    }
+    
+    var firstNumber = ""
+    var secondNumber = ""
+    //it's defined as .none at first
+    var calculateStatus: CalculateStatus = .none
+    
+    //lists for collectionView and for users to caluculate
+    let numbers = [
+        ["C", "%", "$", "÷"],
+        ["7", "8", "9", "×"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", "="]
+    ]
+    
+    @IBOutlet weak var calcCollectionView: UICollectionView!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var collectionViewHeightConstraints: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        calcCollectionView.delegate = self
+        calcCollectionView.dataSource = self
+        calcCollectionView.register(CalculatorViewCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionViewHeightConstraints.constant = view.frame.width * 1.3
+        calcCollectionView.backgroundColor = .clear
+        calcCollectionView.contentInset = .init(top: 0, left: 10, bottom: 0, right: 10)
+        view.backgroundColor = .black
+        numberLabel.text = "0"
     }
-
-
+    
+    func clear() {
+        firstNumber = ""
+        secondNumber = ""
+        numberLabel.text = "0"
+        calculateStatus = .none
+    }
 }
 
+//extension for delegates
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    //how many sections you make in the collection view
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return numbers.count
+    }
+    
+    //how many secrtions inside the collectionView
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numbers[section].count
+    }
+    
+    //methods to define the size of each section
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return .init(width: collectionView.frame.width, height: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var width: CGFloat = 0
+        width = ((collectionView.frame.width - 10) - 10 * 5 ) / 4
+        let height = width
+        if indexPath.section == 4 && indexPath.row == 0 {
+            width = width * 2 + 18
+        }
+        
+        return .init(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = calcCollectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! CalculatorViewCell
+        cell.numberLabel.text = numbers[indexPath.section][indexPath.row]
+        
+        numbers[indexPath.section][indexPath.row].forEach { (numberString) in
+            if "0"..."9" ~= numberString || numberString.description == "." {
+                cell.numberLabel.backgroundColor = .darkGray
+            } else if numberString == "C" || numberString == "%" || numberString == "$" {
+                cell.numberLabel.backgroundColor = UIColor.init(white: 1, alpha: 0.7)
+                cell.numberLabel.textColor = .black
+            }
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let number = numbers[indexPath.section][indexPath.row]
+        
+        switch calculateStatus {
+        case .none:
+            switch number {
+            case "0"..."9":
+                firstNumber += number
+                numberLabel.text = firstNumber
+                if firstNumber.hasPrefix("0") {
+                    firstNumber = ""
+                }
+            case ".":
+                if !decimalPointNeeded(numberString: firstNumber) {
+                    firstNumber += number
+                    numberLabel.text = firstNumber
+                }
+            case "+":
+                calculateStatus = .addition
+            case "-":
+                calculateStatus = .subtraction
+            case "×":
+                calculateStatus = .multiplication
+            case "÷":
+                calculateStatus = .division
+            case "C":
+                clear()
+            default:
+                break
+            }
+            
+        case .addition, .subtraction, .multiplication, .division:
+            switch number {
+            case "0"..."9":
+                secondNumber += number
+                numberLabel.text = secondNumber
+                if secondNumber.hasPrefix("0") {
+                    secondNumber = ""
+                }
+                
+            case ".":
+                if !decimalPointNeeded(numberString: secondNumber) {
+                    secondNumber += number
+                    numberLabel.text = secondNumber
+                }
+                
+            case "=":
+                let firstNum = Double(firstNumber) ?? 0
+                let secondNum = Double(secondNumber) ?? 0
+                var resultString: String?
+                
+                switch calculateStatus {
+                case .addition:
+                    resultString = String(firstNum + secondNum)
+                case .subtraction:
+                    resultString = String(firstNum - secondNum)
+                case .multiplication:
+                    resultString = String(firstNum * secondNum)
+                case .division:
+                    resultString = String(firstNum / secondNum)
+                default:
+                    break
+                }
+                
+                if let result = resultString, result.hasSuffix(".0") {
+                    resultString = result.replacingOccurrences(of: ".0", with: "")
+                }
+                
+                //to reset the value once its calculated, or result will be accumulated then messed up
+                numberLabel.text = resultString
+                firstNumber = ""
+                secondNumber = ""
+                firstNumber += resultString ?? ""
+                calculateStatus = .none
+                
+            case "C":
+                clear()
+            default:
+                break
+            }
+        }
+    }
+        
+    //if decimal point is included or count is 0, it won't add "." never again.
+    private func decimalPointNeeded(numberString: String) -> Bool {
+        if numberString.range(of: ".") != nil || numberString.count == 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+}
+
+class CalculatorViewCell: UICollectionViewCell {
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                self.numberLabel.alpha = 0.3
+            } else {
+                self.numberLabel.alpha = 1
+            }
+        }
+    }
+    
+    let numberLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.text = "1"
+        label.font = .boldSystemFont(ofSize: 32)
+        label.clipsToBounds = true
+        label.backgroundColor = .orange
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(numberLabel)
+        numberLabel.frame.size = self.frame.size
+        numberLabel.layer.cornerRadius = self.frame.height / 2
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
